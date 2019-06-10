a55224b9f476fe2be9afcf115c4151a9b41d40e2
unknown <ioaxaca93@gmail.com>
2019-06-10 13:58:25 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { DirectivasComponent } from './directivas/directivas.component';
+import { PipesExampleComponent } from './pipes-example/pipes-example.component';
+import { FirstC } from './firstc.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'directivas',
+    component: DirectivasComponent
+  },
+  {
+    path:'pipes',
+    component: PipesExampleComponent
+  },
+  {
+    path:'first',
+    component: FirstC
+  },
+  {
+    path:'**',
+    redirectTo: 'first'
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
